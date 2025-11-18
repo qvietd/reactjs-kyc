4d15581e0ef035663f3d7f755765504aec6aa2b7
qvietd <qvietd@gmail.com>
2025-11-18 10:07:32 +0700

    #add lib dependency

---
+++ src/constant/url.ts
@@ -0,0 +1,13 @@
+const AUTH_BASE = "/auth";
+
+export const AUTH_URL = {
+  BASE: AUTH_BASE,
+  LOGIN: `${AUTH_BASE}/login`,
+};
+
+const ADMIN_BASE = "/admin";
+
+export const ADMIN_URL = {
+  BASE: ADMIN_BASE,
+  DASHBOARD: `${ADMIN_BASE}/dashboard`,
+};
